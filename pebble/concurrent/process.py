19bcc94e113209715c765f2422482ce677be420d
wim glenn <hey@wimglenn.com>
2021-02-09 23:57:09 -0600

    naive fix

--- pebble/concurrent/process.py
+++ pebble/concurrent/process.py
@@ -190,7 +190,7 @@ def _function_lookup(name, module):
     except KeyError:  # force function registering
         __import__(module)
         mod = sys.modules[module]
-        getattr(mod, name)
+        _registered_functions[name] = getattr(mod, name)
 
         return _registered_functions[name]
 
